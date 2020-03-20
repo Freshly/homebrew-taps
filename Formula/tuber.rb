3f2b6688d947d7a2521569f6a6032a4949d9a91f
goreleaserbot <goreleaser@carlosbecker.com>
2020-03-20 06:27:56 -0500

    Brew formula update for tuber version v0.1.1

--- Formula/tuber.rb
+++ Formula/tuber.rb
@@ -2,16 +2,16 @@
 class Tuber < Formula
   desc "🥔"
   homepage "https://github.com/Freshly/tuber"
-  version "0.1.0"
+  version "0.1.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/Freshly/tuber/releases/download/v0.1.0/tuber_0.1.0_Darwin_x86_64.tar.gz"
-    sha256 "c0b1f04b43f2ad3984081613b87f3d29c95de41950db49e46c4fb0e1bb154b26"
+    url "https://github.com/Freshly/tuber/releases/download/v0.1.1/tuber_0.1.1_Darwin_x86_64.tar.gz"
+    sha256 "d3daf6f6dcaba9ee0706fb2a751d110bab68052c3abb18aa4899fd0885d19ee4"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/Freshly/tuber/releases/download/v0.1.0/tuber_0.1.0_Linux_x86_64.tar.gz"
-      sha256 "aae8c9c7f4ac6340f93a32a076228388aac797a9124e83fe5a86543a31d48e74"
+      url "https://github.com/Freshly/tuber/releases/download/v0.1.1/tuber_0.1.1_Linux_x86_64.tar.gz"
+      sha256 "f728bc3b7d83c81f94f3256adaf6fd03df3f07abe0900e662f08a7ca73ce49f8"
     end
   end
   
